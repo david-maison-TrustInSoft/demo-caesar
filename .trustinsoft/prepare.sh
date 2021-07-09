c68376d6780775a9f130cfe28f0c8f115988a5c0
David Maison <david.maison@trust-in-soft.com>
2021-07-09 18:14:51 +0200

    poi

---
+++ .trustinsoft/prepare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# apt-get update
+# apt-get install minify
+
+readonly INPUT_FILE=".trustinsoft/orig_config.json"
+readonly COMMON_OPTIONS='{
+   "files": [ "caesar.c", "main.c" ],
+   "cpp-extra-args": "-I .",
+   "prefix_path": ".."
+}'
+
+echo $COMMON_OPTIONS
+
+test -e "$INPUT_FILE" || { echo "$INPUT_FILE not found"; exit 1; }
+
+minify --type js "$INPUT_FILE" \
+  | jq --argjson common "$COMMON_OPTIONS" '.' \
+  > .trustinsoft/config.json
