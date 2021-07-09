f24043e85589794f8108a67ddd599d6ed534acf1
David Maison <david.maison@trust-in-soft.com>
2021-07-09 14:32:14 +0200

    poi

--- .trustinsoft/prepare.sh
+++ .trustinsoft/prepare.sh
@@ -27,4 +27,5 @@ for machdep in "${MACHDEPS[@]}"; do
 done
 
 # Gather all configuration objects into a single one.
+touch .trustinsoft/config.json
 printf '%s\n' "${results[@]}" | jq '.[]' | jq -s > .trustinsoft/config.json
