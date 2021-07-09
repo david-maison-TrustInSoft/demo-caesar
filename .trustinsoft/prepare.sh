4898ccfd2dc68e7665e3286c60b5e18e2b300fe6
David Maison <david.maison@trust-in-soft.com>
2021-07-09 14:50:18 +0200

    poi

--- .trustinsoft/prepare.sh
+++ .trustinsoft/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 apt-get update
-apt-get install jq
+apt-get install lsb-release
 lsb_release -a
 readonly INPUT_FILE=".trustinsoft/orig_config.json"
 readonly MACHDEPS=(
@@ -27,4 +27,4 @@ for machdep in "${MACHDEPS[@]}"; do
 done
 
 # Gather all configuration objects into a single one.
-printf '%s\n' "${results[@]}" | jq '.[]' > .trustinsoft/config.json
+printf '%s\n' "${results[@]}" | jq '.[]' | jq --slurp > .trustinsoft/config.json
